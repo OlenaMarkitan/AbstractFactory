f94d22c0690cc15ce64df96c323b63770565a3bd
OlenaMarkitan <olena.markitan@gmail.com>
2018-12-08 23:42:32 +0200

    Add files via upload

---
+++ Color.java
@@ -0,0 +1,7 @@
+package com.marlen.java.lesson_1.Lesson7_1.Home7.e_olymp.DesignPatterns.Creational_Patterns.AbstractFactory.AbstractFactory_tutorialpoints;
+
+// https://www.tutorialspoint.com/design_pattern/abstract_factory_pattern.htm
+
+public interface Color {
+    void fill();
+}
