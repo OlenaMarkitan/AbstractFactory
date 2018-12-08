efdab8e8c16aef8742f17926e46957c196fae601
OlenaMarkitan <olena.markitan@gmail.com>
2018-12-08 23:41:12 +0200

    Add files via upload

---
+++ AbstractFactory.java
@@ -0,0 +1,8 @@
+package com.marlen.java.lesson_1.Lesson7_1.Home7.e_olymp.DesignPatterns.Creational_Patterns.AbstractFactory.AbstractFactory_tutorialpoints;
+
+// https://www.tutorialspoint.com/design_pattern/abstract_factory_pattern.htm
+
+public abstract class AbstractFactory {
+    abstract Color getColor(String color);
+    abstract Shape getShape(String shape) ;
+}
