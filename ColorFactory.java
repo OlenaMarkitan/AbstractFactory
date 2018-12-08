82f5552a39a900e56f55ee4e0651f0d294de5c36
OlenaMarkitan <olena.markitan@gmail.com>
2018-12-08 23:42:51 +0200

    Add files via upload

---
+++ ColorFactory.java
@@ -0,0 +1,32 @@
+package com.marlen.java.lesson_1.Lesson7_1.Home7.e_olymp.DesignPatterns.Creational_Patterns.AbstractFactory.AbstractFactory_tutorialpoints;
+
+
+// https://www.tutorialspoint.com/design_pattern/abstract_factory_pattern.htm
+
+public class ColorFactory extends AbstractFactory {
+
+    @Override
+    public Shape getShape(String shapeType){
+        return null;
+    }
+
+    @Override
+    Color getColor(String color) {
+
+        if(color == null){
+            return null;
+        }
+
+        if(color.equalsIgnoreCase("RED")){
+            return new Red();
+
+        }else if(color.equalsIgnoreCase("GREEN")){
+            return new Green();
+
+        }else if(color.equalsIgnoreCase("BLUE")){
+            return new Blue();
+        }
+
+        return null;
+    }
+}
